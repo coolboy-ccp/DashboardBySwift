169e2e0e50383948d9746ebd4c24393cecfcbd6f
Brian.Chu <chuchengpeng0119@163.com>
2016-12-21 12:56:54 +0800

    Initial Commit

---
+++ DashboardBySwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DashboardBySwift
+//
+//  Created by 储诚鹏 on 16/12/21.
+//  Copyright © 2016年 RUIYI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
